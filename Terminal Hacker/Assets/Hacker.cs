46b252c45bbd3d2add4f4fd64f9dca16c745845e
strugglebunny <andrew.huntington@gmail.com>
2019-07-06 22:37:24 +0900

    FIrst Changes
    -Imported package from Udemy course
    -Began working on Hacker.cs script

---
+++ Terminal Hacker/Assets/Hacker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu()
+    {
+        var greeting = "Hello Andy";
+
+        Terminal.ClearScreen();
+        Terminal.WriteLine(greeting + "\nWhat would you like to hack into today?\n" +
+                    "1) The local high school\n" +
+                    "2) A large national bank\n" +
+                    "3) NORAD\n" +
+                    "\nPlease enter your selection number: ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
