5b28dd2869232d1a7af3587803e8bea16914bc9e
strugglebunny <andrew.huntington@gmail.com>
2019-07-07 21:04:39 +0900

    Before Final Update and Refactor
    -Added password arrays and randomization
    -Added ASCII art to win screens
    -Add a switch statement to control level selection in StartGame()
    -Etc.

--- Terminal Hacker/Assets/Hacker.cs
+++ Terminal Hacker/Assets/Hacker.cs
@@ -1,21 +1,30 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
     // variables declared outside of functions are Member Variables
-    // game state
+    // Game configureation data
+    string[] level1Passwords = { "gym", "class", "book", "student", "teacher" };
+    string[] level2Passwords = { "account", "withdraw", "finance", "transfer", "interest" };
+    string[] level3Passwords = { "mutually", "assured", "destruction", "America", "Canada" };
+
+    // Game state
     int level;
     enum Screen { MainMenu, Password, Win };
     Screen currentScreen;
+    string password;
 
 
     // Start is called before the first frame update
     void Start()
     {
         ShowMainMenu();
+    }
+  
+    void Update()
+    {
+        // Used to test that Random.Range() works properly
+        //print(Random.Range(0, level1Passwords.Length));
     }
 
     void ShowMainMenu()
@@ -39,22 +48,88 @@ public class Hacker : MonoBehaviour
         {
             RunMainMenu(input);
         }
+        else if (currentScreen == Screen.Password)
+        {
+            RunPasswordCheck(input);
+        }
 
     }
 
-    private void RunMainMenu(string input)
+    void RunPasswordCheck(string input)
     {
-        if (input == "1")
+        if (input == password)
         {
-            level = 1;
+            DisplayWinScreen();
+        }
+        else
+        {
+            Terminal.WriteLine("Password invalid. Please try again.");
+        }
+    }
+
+    void DisplayWinScreen()
+    {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        ShowLevelReward();
+    }
+
+    void ShowLevelReward()
+    {
+        Terminal.WriteLine("Password correct. Accessing network...");
+        switch (level)
+        {
+            case 1:
+                Terminal.WriteLine(@"
+       _.-'`'-._
+    .-'    _    '-.
+     `-.__  `\_.-'
+       |  `-``\|
+       `-.....-A
+               #
+               #
+");
+                break;
+            case 2:
+                Terminal.WriteLine(@"
+|#######====================#######|
+|#(1)*UNITED STATES OF AMERICA*(1)#|
+|#**          /===\   ********  **#|
+|*# {G}      | ( ) |             #*|
+|#*  ******  | /v\ |    O N E    *#|
+|#(1)         \===/            (1)#|
+|##=========ONE DOLLAR===========##|
+------------------------------------
+");
+                break;
+            case 3:
+                Terminal.WriteLine(@"
+          _ ._  _ , _ ._
+        (_ ' ( `  )_  .__)
+      ( (  (    )   `)  ) _)
+     (__ (_   (_ . _) _) ,__)
+         `~~`\ ' . /`~~`
+              ;   ;
+              /   \
+_____________/_ __ \_____________
+");
+                break;
+            default:
+                Debug.LogError("Invalid level number.");
+                break;
+        }
+        
+    }
+
+    void RunMainMenu(string input)
+    {
+        bool isValidLevelNumber = (input == "1" || input == "2" || input == "3");
+        if (isValidLevelNumber)
+        {
+            level = int.Parse(input);
             StartGame();
         }
-        else if (input == "2")
-        {
-            level = 2;
-            StartGame();
-        }
-        else if (input == "007")
+        else if (input == "007") // easter egg
         {
             Terminal.WriteLine("Please choose a level, Mr. Bond: ");
         }
@@ -64,9 +139,30 @@ public class Hacker : MonoBehaviour
         }
     }
 
-    private void StartGame()
+    void StartGame()
     {
         currentScreen = Screen.Password;
-        Terminal.WriteLine("You have chosen level " + level + ".");
+        Terminal.ClearScreen();
+        switch (level)
+        {
+            case 1:
+                password = level1Passwords[Random.Range(0, level1Passwords.Length)];
+                Terminal.WriteLine("Welcome to the LAHS network.\n" +
+                    "Please enter the security password: ");
+                break;
+            case 2:
+                password = level2Passwords[Random.Range(0, level2Passwords.Length)];
+                Terminal.WriteLine("Welcome to the CitiBank network.\n" +
+                    "Please enter the security password: ");
+                break;
+            case 3:
+                password = level3Passwords[Random.Range(0, level3Passwords.Length)];
+                Terminal.WriteLine("Welcome to NORAD.\n" +
+                    "Please enter the security password: ");
+                break;
+            default:
+                Debug.LogError("Invalid level number");
+                break;
+        }
     }
 }
